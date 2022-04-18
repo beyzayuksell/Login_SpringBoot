0845c27e844ee295d01da97b526f20dea484824a
unknown <ybeyza.ankara@gmail.com>
2022-04-19 00:08:39 +0300

    Template of Dependency for Spring Boot Gerekli bağımlılıklar yüklendi.

---
+++ src/main/java/com/beyzayuksel/login_springboot/LoginSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.beyzayuksel.login_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginSpringBootApplication.class, args);
+    }
+
+}
