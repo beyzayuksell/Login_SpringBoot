c6e42ad44ef1b9418357fca7912740083eddfda8
BEYZA <ybeyza.ankara@gmail.com>
2022-04-19 00:45:44 +0300

    Spring security dependency ile gelen login sayfası kaldırıldı. @SpringBootApplication ana proje çalıştırma sayfasında.

--- src/main/java/com/beyzayuksel/login_springboot/LoginSpringBootApplication.java
+++ src/main/java/com/beyzayuksel/login_springboot/LoginSpringBootApplication.java
@@ -3,11 +3,15 @@ package com.beyzayuksel.login_springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// spring security(login page) exclude:dahil etmemek
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+})
+
 public class LoginSpringBootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LoginSpringBootApplication.class, args);
     }
-
 }
