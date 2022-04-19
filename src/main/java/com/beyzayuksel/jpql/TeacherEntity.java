f0921c97030a0d8defcc972b135b0b53395c5fc9
BEYZA <ybeyza.ankara@gmail.com>
2022-04-19 22:07:04 +0300

    TeacherEntity as interface

--- src/main/java/com/beyzayuksel/jpql/TeacherEntity.java
+++ src/main/java/com/beyzayuksel/jpql/TeacherEntity.java
@@ -1,6 +1,8 @@
 package com.beyzayuksel.jpql;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public class TeacherEntity extends CrudRepository<TeacherEntity, Long> {
+@Repository
+public interface TeacherEntity extends CrudRepository<TeacherEntity, Long> {
 }
